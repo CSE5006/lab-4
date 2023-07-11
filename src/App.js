92528ece378908ac06c87708e42a23e20b4501f5
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-12 00:32:01 +1000

    2.2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import List from './components/List';
 import './App.css';
 
 function App() {
+
+    const tasks = [
+        { description: 'Learn React', completed: true },
+        { description: 'Learn JSX', completed: false },
+        { description: 'Build a React App', completed: false }
+    ];
+
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
+            <List heading='My Tasks' tasks={tasks}/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,24 @@
 function Task(props) {
 	return (
-		<li>{ props.description }</li>
+		<li>{ props.description } <input type="checkbox" checked={props.completed} readOnly /></li>
 	);
 }
 
 function List(props) {
+
+	let tasks = [];
+
+	for (let i = 0; i < props.tasks.length; i++) {
+		let description = props.tasks[i].description;
+		let completed = props.tasks[i].completed;
+		tasks.push(<Task description={description} completed={completed} />);
+	}
+
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<Task description='Learn React'/>
-				<Task description='Learn JSX'/>
-				<Task description='Build a React App'/>
+				{ tasks }
 			</ul>
 		</div>
 	);
