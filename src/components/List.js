6d55efa2f8e7ab94ec08643b76a1d4e2b64e36ed
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-12 00:22:41 +1000

    Recreated (#1)
    * init
    
    * Basic List
    
    * New Website

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+function List(props) {
+	return (
+		<div>
+			<h1>{ props.heading }</h1>
+			<ul>
+				<li>Learn React</li>
+				<li>Learn JSX</li>
+				<li>Build a React App</li>
+			</ul>
+		</div>
+	);
+}
+
+export default List;
